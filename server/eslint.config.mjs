62b106e631c51f59e61f04931d85649050bab3a5
Lakshyalamba <choudharylakshya758@gmail.com>
2026-05-14 23:25:46 +0530

    Configure Express server workspace

---
+++ server/eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.node
+      },
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules
+    }
+  }
+];
